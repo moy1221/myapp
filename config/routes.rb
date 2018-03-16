fc682887045d697bdc0a35cb83880a63becd7ade
Moises Nuñez <manc1221@gmail.com>
2018-03-16 16:44:49 -0600

    revert commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
   get 'welcome/about', to: 'welcome#about'
