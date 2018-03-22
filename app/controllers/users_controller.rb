df819410950d62746d51012d4689da2395277dd4
Moises Nuñez <manc1221@gmail.com>
2018-03-22 16:23:21 -0600

    Login and logut added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -20,12 +21,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
-    @user_articles = @user..paginate(page: params[:page], per_page: 5)
+    @user_articles = @user.paginate(page: params[:page], per_page: 5)
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Successfully updated"
       redirect_to articles_path
@@ -34,14 +33,16 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
