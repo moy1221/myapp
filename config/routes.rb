e712cf4c7cd0033dc37a7d3401a5f25878c2ff63
Moises Nuñez <manc1221@gmail.com>
2018-03-22 11:22:00 -0600

    Create user view added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
   get 'welcome/about', to: 'welcome#about'
 
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
