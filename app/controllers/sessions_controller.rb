860460a9ebd825faf388f7b5b5cafa90510a9172
Moises Nuñez <manc1221@gmail.com>
2018-03-23 09:10:30 -0600

    Sign in upon signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:sucess] = "you have logged out"
+    flash[:success] = "you have logged out"
     redirect_to root_path
   end
 end
