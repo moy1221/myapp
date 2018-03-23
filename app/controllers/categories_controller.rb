58cbf1f3dfe7ca3fa9510ee6c2378a50f32434f1
Moises Nuñez <manc1221@gmail.com>
2018-03-23 16:16:30 -0600

    Add admin test (two are not working)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
+
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
@@ -10,7 +12,7 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      flash[:success] = "Category was create"
+      flash[:success] = "Category was created"
       redirect_to categories_path
     else
       render 'new'
@@ -24,6 +26,13 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin?)
+      flash[:danger] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
   end
 
 end
